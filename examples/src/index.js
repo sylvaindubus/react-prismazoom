725327b4d4ab91ac30f9a6aa025faf9f253f100f
Sylvain Dubus <svn.dbs@gmail.com>
2021-02-26 18:53:38 +0100

    Rework on the example page

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,8 +2,7 @@ import React, { Component, createRef } from 'react'
 import { render } from 'react-dom'
 
 import PrismaZoom from '../../src'
-import sealHorizontal from './seal-horizontal.jpg'
-import sealVertical from './seal-vertical.jpg'
+import background from './radeau-de-la-meduse.jpg'
 import './styles.css'
 
 class App extends Component {
@@ -11,9 +10,7 @@ class App extends Component {
     super(props)
     this.prismaZoom = createRef()
     this.state = {
-      zoom: 1,
-      posX: 0,
-      posY: 0
+      zoom: 1
     }
   }
 
@@ -22,7 +19,7 @@ class App extends Component {
   }
 
   onPanChange = pos => {
-    this.setState({ ...pos })
+    console.log(pos.posX, pos.posY)
   }
 
   onClickOnZoomOut = () => {
@@ -66,23 +63,24 @@ class App extends Component {
           <h2>A pan and zoom component for React, using CSS transformations.</h2>
         </header>
         <section className="App-wrapper">
-          <PrismaZoom className="App-zoom" topBoundary={120} onZoomChange={this.onZoomChange} onPanChange={this.onPanChange} ref={this.prismaZoom}>
-            <img className="App-image onDesktop" src={sealHorizontal} alt="A cute seal" />
-            <img className="App-image onMobile" src={sealVertical} alt="A cute seal on mobile" />
-            <div className="App-card" onDoubleClick={this.onDoubleClickOnCard}>
-              <h3>Double-click on this card ;-)</h3>
-              <p>Fur seals are any of nine species of pinnipeds belonging to the subfamily Arctocephalinae in the family Otariidae. They are much more closely related to sea lions than true seals, and share with them external ears (pinnae), relatively long and muscular foreflippers, and the ability to walk on all fours. They are marked by their dense underfur, which made them a long-time object of commercial hunting.</p>
-              <p>Eight species belong to the genus Arctocephalus and are found primarily in the Southern Hemisphere, while a ninth species also sometimes called fur seal, the northern fur seal (Callorhinus ursinus), belongs to a different genus and inhabits the North Pacific.</p>
-              <p><a href="https://en.wikipedia.org/wiki/Fur_seal" target="_blank" rel="noreferrer">https://en.wikipedia.org/wiki/Fur_seal</a></p>
-            </div>
+          <PrismaZoom className="App-zoom" topBoundary={120} onZoomChange={this.onZoomChange} maxZoom={8} onPanChange={this.onPanChange} ref={this.prismaZoom}>
+            <div className="App-image" style={{ backgroundImage: `url(${background})` }}></div>
+            <article className="App-card" onDoubleClick={this.onDoubleClickOnCard}>
+              <h3>The Raft of the Medusa</h3>
+              <p>The Raft of the Medusa (French: Le Radeau de la Méduse) – originally titled Scène de Naufrage (Shipwreck Scene) – is an oil painting of 1818–19 by the French Romantic painter and lithographer Théodore Géricault (1791–1824). Completed when the artist was 27, the work has become an icon of French Romanticism.</p>
+              <p><a href="https://en.wikipedia.org/wiki/The_Raft_of_the_Medusa" target="_blank" rel="noreferrer">Go to Wikipedia.</a></p>
+              <footer><strong>Tip: </strong>double-click on this card to zoom. 😉</footer>
+            </article>
           </PrismaZoom>
         </section>
         <footer className="App-footer">
           <div className="App-indicator">
-            <button className="App-button" onClick={this.onClickOnZoomOut}>-</button>
-            <span className="App-zoomLabel">{`Zoom: ${parseInt((this.state.zoom * 100))}%`}</span>
-            <button className="App-button" onClick={this.onClickOnZoomIn}>+</button>
-            <span className="App-posLabel">{`X: ${parseInt((this.state.posX))} Y: ${parseInt((this.state.posY))}`}</span>
+            <button className="App-button" onClick={this.onClickOnZoomOut}>
+              <svg className="App-buttonIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13H5v-2h14v2z"/></svg></button>
+            <span>{parseInt(this.state.zoom * 100)}%</span>
+            <button className="App-button" onClick={this.onClickOnZoomIn}>
+              <svg className="App-buttonIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
+            </button>
           </div>
         </footer>
       </div>
